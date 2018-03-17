88f6f732b57fbca8c1363f77c2b2fa6de6660e37
Darrell Washington <darrellwjr.com@gmail.com>
2018-03-17 14:23:27 -0400

    Initial Commit Readme will come later

---
+++ js/todo-app.js
@@ -0,0 +1,60 @@
+var app = new Vue({
+    el: "#app",
+    data: {
+        todos: [
+            {id: Math.random(), text: "Learn JavaScript" },
+            {id: Math.random(), text: "Learn Vue" },
+            {id: Math.random(), text: "Build something awesome" }
+        ],
+        input: "",
+        editing: false,
+        editingID: ""
+    },
+    computed: {
+    },
+    watch: {
+        editingID: function (newID, oldID) {
+            updateTodos = this.todos.filter(todo => todo.id !== newID);
+            this.todos = updateTodos;
+        }
+    },
+    methods: {
+        clearInput: function() {
+            this.input = "";
+        },
+        addTodo: function () {
+            if(this.editing) {
+                this.todos.push({
+                    id: this.editingID,
+                    text: this.input
+                });
+                this.editing = false;
+            } else if(this.input !== "") {
+                this.todos.push({
+                    id: Math.random(),
+                    text: this.input
+                });
+            }
+            this.clearInput();
+        },
+        deleteTodo: function (id) {
+            let newTodos = this.todos.filter(todo => todo.id !== id);
+            this.todos = newTodos;
+        },
+        findValue: function (element) {
+            return element !== undefined;
+        },
+        editTodo: function(id) {
+            this.editingID = id;
+            let editingTodo =this.todos.map(todo => {
+                    if(todo.id === id) {
+                        return todo.text;
+                    }
+                });
+            editingTodo.splice(0,editingTodo.findIndex(this.findValue));
+            this.input = editingTodo[0];
+            this.editing = true;
+        }
+    }
+
+});
