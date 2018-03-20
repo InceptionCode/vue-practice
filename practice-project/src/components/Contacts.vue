06fae5b342f6c77621dcbec50331c26b66f5f385
Darrell Washington <darrellwjr.com@gmail.com>
2018-03-19 21:13:39 -0400

    Added basic functionality to Contact Book app. Both adding and deleting methods are basic and not at final structure.

--- practice-project/src/components/Contacts.vue
+++ practice-project/src/components/Contacts.vue
@@ -1,39 +1,59 @@
 <template>
-  <header>
+  <div>
     <h1>{{title}}</h1>
+    <form @keyup.prevent.enter="addContact">
+      <label>Add Name</label>
+      <input type="text" 
+      v-model = "inputName">
+      <label>Add Number</label>
+      <input type="text"
+      v-model = "inputNumber">
+      <label>Add Email</label>
+      <input type="email"
+      v-model = "inputEmail">
+    </form>
     <ul>
-      <li v-for= "contact in contacts"
+      <li v-for= "contact in contactList"
           :key = "contact.number">
         {{contact.name}}: {{contact.number}}
         <br>
         {{contact.email}}
+        <span @click="deleteContact(contact.number)">X</span>
       </li>
     </ul>
-  </header>
+    <button @click="addContact">Add Contact</button>
+  </div>
 </template>
 
 <script>
 export default {
+  props: ['contacts'],
   data() {
     return {
       title: "Contact Book",
-      contacts: [
-        {
-          name: "Darrell",
-          number: "1-677-444-4449",
-          email: "darrellwjr.com@gmail.com",
-        },
-        {
-          name: "tamia",
-          number: "1-634-564-4949",
-          email: "darrellwjr.com@gmail.com",
-        },
-        {
-          name: "Kevin",
-          number: "1-627-344-5449",
-          email: "kevinfake@fakemail.com",
-        }
-      ] 
+      contactList: this.contacts,
+      inputName: '',
+      inputNumber: '',
+      inputEmail: '' 
+    }
+  },
+  methods: {
+    resetInputs: function () {
+      this.inputName = '';
+      this.inputNumber = '';
+      this.inputEmail= '';
+    },
+    addContact: function () {
+      this.contactList.push({
+        name: this.inputName,
+        number: this.inputNumber,
+        email: this.inputEmail
+      });
+      this.resetInputs();
+    },
+    deleteContact: function(key) {
+      let filterContacts = this.contactList.filter(contact => contact.number !== key);
+      this.contactList = filterContacts;
     }
   }
 }
--- practice-project/src/components/Main.vue
+++ practice-project/src/components/Main.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <app-header></app-header>
-    <contacts></contacts>
+    <contacts :contacts="contacts"></contacts>
     <app-footer></app-footer>
   </div>
 </template>
@@ -20,6 +20,23 @@ export default {
   },
   data() {
     return {
+      contacts: [
+        {
+          name: "Darrell",
+          number: "1-677-444-4449",
+          email: "darrellwjr.com@gmail.com",
+        },
+        {
+          name: "tamia",
+          number: "1-634-564-4949",
+          email: "queenT@fakemail.com",
+        },
+        {
+          name: "Kevin",
+          number: "1-627-344-5449",
+          email: "kevinfake@fakemail.com",
+        }
+      ] 
     }
   }
 }
