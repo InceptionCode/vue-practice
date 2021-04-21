e6c7337b72b22f51d8bb34f9be40731d3f32d930
Darrell Washington <darrellwjr.com@gmail.com>
2021-04-20 21:38:34 -0400

    refactored project to include vue-enterprise boilerplate. Finished todo-app.

---
+++ frontend/vue-todo/bs-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "port": 3000,
+  "files": ["./html/*.{html}", "./css/*.{css}", "./js/*.{js}"],
+  "server": {
+    "baseDir": "./", 
+  },
+  "startPath": "./html/todo.html" 
+}
