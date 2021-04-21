947562d0909e767652294f6f47e27cd4d845f573
Darrell Washington <darrellwjr.com@gmail.com>
2021-04-20 21:39:02 -0400

    refactored project to include vue-enterprise boilerplate. Finished todo-app.

--- blog-vue/plugins/core-components.js
+++
@@ -1,9 +0,0 @@
-import Vue from 'vue';
-
-import AppPostsSection from '@/components/posts/AppPostsSection'
-import AppControlInput from '@/components/ui/AppControlInput'
-import AppButton from '@/components/ui/AppButton'
-
-Vue.component('app-posts-section', AppPostsSection)
-Vue.component('app-control-input', AppControlInput)
-Vue.component('app-button', AppButton)
--- bs-config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  "port": 8000,
-  "files": ["./html/*.{html}", "./css/*.{css}", "./js/*.{js}"],
-  "server": {
-    "baseDir": "./", 
-  },
-  "startPath": "./html/todo.html" 
-}
